171a0bc0146b0ff50bd790e77f2158c1246c00c7
gtx70 <>
2024-02-02 19:08:21 +0800

    Adjust app consts.

--- SourceCode/SharpD12/Core/AppConstants.cs
+++ SourceCode/SharpD12/Core/AppConstants.cs
@@ -5,21 +5,17 @@ namespace SharpD12
 {
   public static class AppConstants
   {
-    /// <summary>
-    /// Best anisotropy level value that is good for both performance and quality.
-    /// </summary>
+    /// <summary>Best anisotropy level value considering both performance and quality.</summary>
     public const int AnisotropyLevel = 4;
 
     public const int SwapChainSize = 3;
 
-    /// <summary>
-    /// 11_0 feature level in DX12 can support GPU down to GeForce 400 series!
-    /// </summary>
+    /// <summary>11_0 feature level in DX12 can support GPU down to GeForce 400 series!</summary>
     public const FeatureLevel DX12FeatureLevel = FeatureLevel.Level_11_0;
 
     public static readonly Color4 CleanColor = new Color4(0.2f, 0.2f, 0.21f, 0f);
     
-    public const float GameUnit2Meter = 1000f;
+    public const float UnitToMeter = 100f;
 
     public const int MaxStaticRenderItems = 1 << 10;
 
