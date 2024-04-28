acfa653deeb16f8d5fa8f07b704f5e37b13da111
gtx70 <>
2024-04-28 23:49:45 +0800

    Update SD12Form, hide useless properties

--- SourceCode/SharpD12/Program.cs
+++ SourceCode/SharpD12/Program.cs
@@ -4,79 +4,85 @@ using SharpDX;
 using SharpDX.Windows;
 using Linearstar.Windows.RawInput;
 
-namespace SharpD12
+namespace SharpD12;
+using SharpDX.DXGI;
+using System.Drawing;
+
+static class Program
 {
-  using System.Drawing;
-  static class Program
+  /// <summary>
+  /// Entry point of application.
+  /// </summary>
+  [STAThread]
+  static void Main()
   {
-    /// <summary>
-    /// Entry point of application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    var form = new SD12Form();
+    form.SetClientSize(1920, 1080);
+    form.Show();
+    try
     {
-      var form = new CustomedForm();
-      form.SetContentSize(1920, 1080);
-      form.Show();
-      try
-      {
-        var engine = new SD12Engine(form);
-        engine.Run();
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show(ex.ToString(), "Fatal Error", MessageBoxButtons.OK);
-      }
+      var engine = new SD12Engine(form);
+      engine.Run();
+    }
+    catch (Exception ex)
+    {
+      MessageBox.Show(ex.ToString(), "Fatal Error", MessageBoxButtons.OK);
     }
   }
+}
 
-  public class CustomedForm : RenderForm
+public sealed class SD12Form : RenderForm
+{
+  // Hide useless properties.
+  new bool IsFullscreen { get; }
+  new bool AllowUserResizing { get; }
+
+  Rectangle prevWinRect;
+  Action<RawInputData> inputEvent;
+  static Size minSize = new Size(400, 400);
+
+  public SD12Form() : base()
   {
-    Rectangle preWinRect;
-    Action<RawInputData> inputEvent;
-    static string defaultName = "SharpD12";
-    static Size minSize = new Size(400, 400);
-    
-    public CustomedForm() : base()
+    Size = minSize;
+    Text = "DefaultTitle";
+    MinimumSize = minSize;
+    SetWindowMode(false);
+  }
+
+  /// <summary>
+  ///  ClientSize excludes border size. 
+  /// </summary>
+  public void SetClientSize(int width, int height) => ClientSize = new Size(width, height);
+
+  public void SetWindowMode(bool isFullScreenForm, bool allowResizing = true)
+  {
+    var targetRect = new Rectangle();
+    if (isFullScreenForm)
     {
-      Text = defaultName;
-      Size = minSize;
-      MinimumSize = minSize;
-      AllowUserResizing = true;
+      FormBorderStyle = FormBorderStyle.None;
+      prevWinRect = new Rectangle(Location, ClientSize);
+      targetRect = Screen.FromControl(this).Bounds;
     }
-
-    public void SetContentSize(int width, int height) => ClientSize = new Size(width, height); // ClientSize excludes border size. 
-
-    public void SetWindowMode(bool fullScreen, bool allowResizing = true)
+    else
     {
-      AllowUserResizing = allowResizing;
-      var targetRect = new Rectangle();
-      if (fullScreen)
-      {
-        FormBorderStyle = FormBorderStyle.None;
-        preWinRect = new Rectangle(Location, ClientSize);
-        targetRect = Screen.FromControl(this).Bounds;
-      }
-      else
-      {
-        FormBorderStyle = allowResizing ? FormBorderStyle.Sizable : FormBorderStyle.FixedSingle;
-        targetRect = preWinRect;
-      }
-      ClientSize = targetRect.Size;
-      Location = targetRect.Location;
+      base.MaximizeBox = allowResizing;
+      FormBorderStyle = allowResizing ? FormBorderStyle.Sizable : FormBorderStyle.FixedSingle;
+      targetRect = prevWinRect;
     }
+    ClientSize = targetRect.Size;
+    Location = targetRect.Location;
+  }
 
-    public void SetInputEvent(Action<RawInputData> act) => inputEvent = act;
+  public void SetInputEvent(Action<RawInputData> act) => inputEvent = act;
 
-    protected override void WndProc(ref Message m)
+  protected override void WndProc(ref Message m)
+  {
+    base.WndProc(ref m);
+    const int WM_INPUT = 0x00FF;
+    if (m.Msg == WM_INPUT)
     {
-      base.WndProc(ref m);
-      const int WM_INPUT = 0x00FF;
-      if (m.Msg == WM_INPUT)
-      {
-        var data = RawInputData.FromHandle(m.LParam);
-        inputEvent?.Invoke(data);
-      }
+      var data = RawInputData.FromHandle(m.LParam);
+      inputEvent?.Invoke(data);
     }
   }
 }
