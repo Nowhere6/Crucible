daff3b2e22c2ac24c019a231390036438c49db42
gtx70 <>
2024-06-23 12:40:46 +0800

    Add Illumination.cs

---
+++ SourceCode/Crucible/Core/Illumination.cs
@@ -0,0 +1,25 @@
+using SharpDX;
+
+namespace Crucible;
+
+public static class SphereHarmonicOrder3
+{
+
+}
+
+public enum CubemapSize : int
+{
+  x16 = 16,
+  x32 = 32,
+  x64 = 64,
+  x128 = 128,
+  x256 = 256
+}
+
+public static class RadianceCubemap
+{
+  public static void MakeCubemap(Vector3 center, CubemapSize cubemapSize)
+  {
+
+  }
+}
