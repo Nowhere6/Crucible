89f2a60cbd042394c0e2e5b5ce181c20aa8d76aa
gtx70 <>
2024-06-29 21:53:21 +0800

    Update EngineInit.cs

--- SourceCode/Crucible/Core/EngineInit.cs
+++ SourceCode/Crucible/Core/EngineInit.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using SharpDX;
 using SharpDX.DXGI;
 using SharpDX.Direct3D12;
-using static SharpD12.AppConstants;
+using static Crucible.AppConstants;
 using D12Device = SharpDX.Direct3D12.Device;
 using D12Resource = SharpDX.Direct3D12.Resource;
 
-namespace SharpD12;
+namespace Crucible;
 
 public partial class SD12Engine
 {
@@ -76,7 +76,7 @@ public partial class SD12Engine
       SwapEffect = SwapEffect.FlipDiscard,
       Flags = SwapChainFlags.AllowTearing,
       SampleDescription = new SampleDescription(1, 0),
-      ModeDescription = new ModeDescription(Format.R8G8B8A8_UNorm),
+      ModeDescription = new ModeDescription(Format.R8G8B8A8_UNorm)
     };
     swapChain = new SwapChain(factory, commandQueue, description);
   }
@@ -87,18 +87,18 @@ public partial class SD12Engine
     DescHeapManager.Initialize(dx12Device);
 
     // Create constant buffer and pass cbv.
-    FrameResource.passBuffer = new UploadBuffer<SuperPassConsts>(dx12Device, SwapChainSize, true);
-    FrameResource.staticRenderItemObjectBuffer = new UploadBuffer<SuperObjectConsts>(dx12Device, MaxStaticRenderItems * SwapChainSize, true);
-    FrameResource.uiRenderItemObjectBuffer = new UploadBuffer<SuperObjectConsts>(dx12Device, MaxUIRenderItems * SwapChainSize, true);
+    FrameResource.passBuffer = new UploadBuffer<SuperPassConsts>(dx12Device, SwapChainSize, BufferType.ConstantBuffer);
+    FrameResource.staticRenderItemObjectBuffer = new UploadBuffer<SuperObjectConsts>(dx12Device, MaxStaticRenderItems * SwapChainSize, BufferType.ConstantBuffer);
+    FrameResource.uiRenderItemObjectBuffer = new UploadBuffer<SuperObjectConsts>(dx12Device, MaxUIRenderItems * SwapChainSize, BufferType.ConstantBuffer);
 
     PipelineStateManager.Initialize(dx12Device);
   }
 
   void CreateFrames()
   {
+    // Create frame resources, pass resources, command allocator.
+    RenderPassResource.Reinitialize(dx12Device, width, height);
     frames = new FrameResource[SwapChainSize];
-
-    // Create RTV and command allocator.
     var rtvDesc = new RenderTargetViewDescription { Format = Format.R8G8B8A8_UNorm, Dimension = RenderTargetViewDimension.Texture2D };
     foreach (int i in Enumerable.Range(0, SwapChainSize))
     {
@@ -122,7 +122,8 @@ public partial class SD12Engine
 
   void LoadTextures()
   {
-    Texture.Load_PNG_RGBA32_AutoMip(dx12Device, PathHelper.GetPath(@"Textures\Default.png"), "Default");
+    Texture.Load_RGBA32AutoMip(dx12Device, PathHelper.GetPath(@"Textures\Default.png"), "Default");
+    Texture.Load_RGBA32AutoMip(dx12Device, PathHelper.GetPath(@"Textures\can_n.png"), "Normal");
   }
 
   void BuildRenderItems()
@@ -136,8 +137,9 @@ public partial class SD12Engine
     renderItem.objectConst = new SuperObjectConsts { world = Matrix.Identity };
     //renderItem.mesh = MeshManager.CreateBox(dx12Device, 1, 1, 1);
     // FRAME DROP because big upload heap.
-    renderItem.mesh = MeshManager.LoadExternalModel(dx12Device, "dragon.obj");
+    renderItem.mesh = MeshManager.LoadExternalModel(dx12Device, "can.fbx");
     renderItem.albedoTex = "Default";
+    renderItem.normalTex = "Normal";
 
     // Temp
     var uiRenderItem = new UIRenderItem();
